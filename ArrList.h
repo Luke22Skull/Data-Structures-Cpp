d06231758bd99a5de8219ad2da156e6af5a1b030
iLukeDuke <lukashi2209@gmail.com>
2025-03-13 14:48:54 +0100

    Commented methods

--- ArrList.h
+++ ArrList.h
@@ -3,31 +3,37 @@
 
 #include <iostream>
 #include <stdexcept>
-const int MAX_SIZE = 100;
+const int MAX_SIZE = 100;   // Max List size
 using namespace std;
 
 // LIST - Array
 template <typename T>
 class ArrList {
 private:
-    T array[MAX_SIZE];
-    int size;
+    T array[MAX_SIZE];  // Actual List
+    int size;   // size of the array
 
 public:
+    // Constructor: initialize array with size = 0
     ArrList() : size(0) {}
+
+    // Destructor
     ~ArrList() {
         while (!isEmptyList())
             deleteList(0);
     }
 
+    // getter: gets the size of the array
     int getSize() const {
         return size;
     }
 
+    // checks if the list is empty
     bool isEmptyList() const {
         return size == 0;
     }
 
+    // reads the value at a given index
     T readValueAtIndex(int index) const {
         if (index < 0 || index >= size) {
             throw out_of_range("Index error");
@@ -35,6 +41,7 @@ public:
         return array[index];
     }
 
+    // modifies the value at a given index with a new value
     void writeValue(T value, int index) {
         if (index < 0 || index >= size) {
             throw out_of_range("Index error");
@@ -42,14 +49,17 @@ public:
         array[index] = value;
     }
 
+    // gets the first element in list
     T firstInList() const {
         return array[0];
     }
 
+    // checks if the list is full
     bool endList(int index) const {
         return index == size;
     }
-
+    
+    // gets the next value in the array with a given index
     T nextValue(int index) const {
         if (index < 0 || index >= size) {
             throw out_of_range("Index error");
@@ -57,6 +67,7 @@ public:
         return array[index + 1];
     }
 
+    // gets the previous value in the array with a given index
     T previousValue(int index) const {
         if (index <= 0 || index > size) {
             throw out_of_range("Index error");
@@ -64,6 +75,7 @@ public:
         return array[index - 1];
     }
 
+    // shifts values and inserts a new value
     void insertValue(T value, int index) {
         if (index < 0 || index > size || size >= MAX_SIZE) {
             throw out_of_range("Index error");
@@ -75,6 +87,7 @@ public:
         ++size;
     }
 
+    // deletes value at a given index, then shifts
     void deleteList(int index) {
         if (index < 0 || index >= size) {
             throw out_of_range("Index error");
@@ -85,6 +98,7 @@ public:
         --size;
     }
 
+    // prints the list
     void printList() const {
         for (int i = 0; i < size; ++i) {
             cout << array[i] << " ";
